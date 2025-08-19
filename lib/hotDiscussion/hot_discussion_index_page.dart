30ac6e2495ed0773ea70cfaa2f1004db98c3adae
reed <reed@qq.com>
2025-08-19 16:11:15 +0800

    update

---
+++ lib/hotDiscussion/hot_discussion_index_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:hlw/base/gen_custom_nav.dart';
+import 'package:hlw/util/desktop_extension.dart';
+import 'package:hlw/util/easy_pull_refresh.dart';
+import 'package:hlw/util/local_png.dart';
+import 'package:hlw/util/utils.dart';
+import 'package:hlw/widget/search_bar_widget.dart';
+
+import '../util/style_theme.dart';
+import 'history_content_page.dart';
+
+class HotDiscussionIndexPage extends StatefulWidget {
+  final dynamic args;
+
+  const HotDiscussionIndexPage({super.key, this.args});
+
+  @override
+  State createState() => _HotDiscussionIndexPageState();
+}
+
+class _HotDiscussionIndexPageState extends State<HotDiscussionIndexPage> {
+  DateTime? selectedDate = DateTime.now();
+
+  int tabSelectIndex = 2;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: [
+      Positioned(
+        top: 90.w,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        child: _buildHistoryWidget(),
+      ),
+      const SearchBarWidget(),
+    ]);
+  }
+
+  Widget _buildHistoryWidget() {
+    return GenCustomNav(
+      isCenter: false,
+      titles: ["今日话题", "历史话题"],
+      pages: [
+        Container(),
+        Container(),
+      ],
+    );
+  }
+}
